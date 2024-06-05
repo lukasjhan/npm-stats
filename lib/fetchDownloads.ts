841135794475448d4b65a96188b62dd00ed7b17e
Lukas <Lukas@hopae.io>
2024-06-05 18:57:23 +0900

    chore: remove unused files

--- lib/fetchDownloads.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export async function fetchDownloads(packageName: string) {
-  const response = await axios.get(
-    `https://api.npmjs.org/downloads/range/last-year/${packageName}`,
-  );
-  const data = response.data;
-  return data;
-}
